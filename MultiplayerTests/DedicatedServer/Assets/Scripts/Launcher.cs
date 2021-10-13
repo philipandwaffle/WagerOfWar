dc4d36f302e2f72276d5056d6f5324dd8146ab28
philipandwaffle <philipcox197@gmail.com>
2021-10-13 10:23:31 +0100

    Multiplayer Stuff

--- MultiplayerTests/DedicatedServer/Assets/Scripts/Launcher.cs
+++ MultiplayerTests/DedicatedServer/Assets/Scripts/Launcher.cs
@@ -7,7 +7,13 @@ using Photon.Realtime;
 public class Launcher : MonoBehaviourPunCallbacks
 {
     #region Private Serializable Fields
+    [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+    [SerializeField]
+    private GameObject controlPanel;
 
+    [Tooltip("The UI Label to inform the user that the connection is in progress")]
+    [SerializeField]
+    private GameObject progressLabel;
     #endregion
 
     #region Private Fields
@@ -23,11 +29,16 @@ public class Launcher : MonoBehaviourPunCallbacks
     /// <summary>
     /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
     /// </summary>
-    void Awake()
+    private void Awake()
     {
         // #Critical
         // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
         PhotonNetwork.AutomaticallySyncScene = true;
+    }
+    private void Start()
+    {
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
     }
     #endregion
 
@@ -44,6 +55,8 @@ public class Launcher : MonoBehaviourPunCallbacks
     public override void OnDisconnected(DisconnectCause cause)
     {
         Debug.LogWarningFormat($"PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {cause}");
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
     }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
@@ -71,6 +84,8 @@ public class Launcher : MonoBehaviourPunCallbacks
     /// </summary>
     public void Connect()
     {
+        progressLabel.SetActive(true);
+        controlPanel.SetActive(false);
         // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
         if (PhotonNetwork.IsConnected)
         {
