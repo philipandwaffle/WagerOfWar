da67f66c1356fb20f6edaae3594ab0c460770d8f
Philip Cox <philipcox197@gmail.com>
2021-10-16 09:45:53 +0100

    Multiplayer

---
+++ MultiplayerTests/DedicatedServer/Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimatorManager : MonoBehaviour
+{
+    #region Private Fields
+    [SerializeField]
+    private float directionDampTime = 0.25f;
+
+    private Animator animator;
+    #endregion
+    
+    #region MonoBehaviour Callbacks
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        if (!animator)
+        {
+            Debug.LogError("PlayerAnimatorManager is missing Animator component", this);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!animator)
+        {
+            return;
+        }
+
+        // deal with Jumping
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        // only allow jumping if we are running.
+        if (stateInfo.IsName("Base Layer.Run"))
+        {
+            // When using trigger parameter
+            if (Input.GetButtonDown("Fire2"))
+            {
+                animator.SetTrigger("Jump");
+            }
+        }
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        if (v < 0)
+        {
+            v = 0;
+        }
+        animator.SetFloat("Speed", h * h + v * v);
+        animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+    }
+
+
+    #endregion
+}
