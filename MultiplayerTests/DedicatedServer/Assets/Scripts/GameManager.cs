3309c8f5f4233fe88519d2ece2c4c00e6fe41f2c
philipandwaffle <philipcox197@gmail.com>
2021-12-02 12:30:34 +0000

    Multiplayer stuff

--- MultiplayerTests/DedicatedServer/Assets/Scripts/GameManager.cs
+++ MultiplayerTests/DedicatedServer/Assets/Scripts/GameManager.cs
@@ -7,6 +7,25 @@ using Photon.Realtime;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
+    public static GameManager Instance;
+    [Tooltip("The prefab to use for representing the player")]
+    public GameObject playerPrefab;
+
+    void Start()
+    {
+        Instance = this;
+        if (playerPrefab == null)
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+        }
+        else
+        {
+            Debug.LogFormat("We are Instantiating LocalPlayer from {0}", Application.loadedLevelName);
+            // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+            PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+        }
+    }
+
     #region Photon Callbacks
     /// <summary>
     /// Called when the local player left the room. We need to load the launcher scene.
--- MultiplayerTests/DedicatedServer/Assets/Scripts/PlayerAnimatorManager.cs
+++ MultiplayerTests/DedicatedServer/Assets/Scripts/PlayerAnimatorManager.cs
@@ -1,8 +1,9 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimatorManager : MonoBehaviour
+public class PlayerAnimatorManager : MonoBehaviourPun
 {
     #region Private Fields
     [SerializeField]
@@ -26,6 +27,10 @@ public class PlayerAnimatorManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
+        {
+            return;
+        }
 
         if (!animator)
         {
